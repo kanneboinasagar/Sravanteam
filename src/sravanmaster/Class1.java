ff2d77c59f7fea68235d7907c228f6aceb30ffbc
Sravan Kumar <mammula.sravan42384@gmail.com>
2023-01-05 16:59:27 +0530

    changes done

--- src/sravanmaster/Class1.java
+++ src/sravanmaster/Class1.java
@@ -4,7 +4,7 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		System.out.println("hello sagar");
-        System.out.println("hello sravani");
+        System.out.println("hello ravani");
 	}
 
 }
