76fa39b48806ff5d8b8713d50feed342027086ea
Sravan Kumar <mammula.sravan42384@gmail.com>
2023-01-05 16:16:22 +0530

    Welcome to my group guys

---
+++ src/sravanmaster/Class1.java
@@ -0,0 +1,10 @@
+package sravanmaster;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		System.out.println("hello sagar");
+        System.out.println("hello sravani");
+	}
+
+}
