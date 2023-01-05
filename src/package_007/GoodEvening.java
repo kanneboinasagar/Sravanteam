fb2697c50373ac9a9371ba51b3d5920e767f937d
Kanneboina Sagar Babu <ksbabu96@gmail.com>
2023-01-05 16:41:10 +0530

    Changes had been made

--- src/package_007/GoodEvening.java
+++ src/package_007/GoodEvening.java
@@ -1,9 +1,9 @@
 package package_007;
 
-public class Goodmorning
+public class GoodEvening
 {
 	public static void main(String[] args) {
-		System.out.println("GoodMorning");
+		System.out.println("GoodEvening");
 	}
 
 }
