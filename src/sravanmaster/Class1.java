a4fa2eb89361f7a961dbafb603b173acd7b92c0b
kanneboinasagar <121668338+kanneboinasagar@users.noreply.github.com>
2023-01-05 16:57:04 +0530

    Update Class1.java
    Updated the output

--- src/sravanmaster/Class1.java
+++ src/sravanmaster/Class1.java
@@ -3,8 +3,8 @@ package sravanmaster;
 public class Class1 {
 
 	public static void main(String[] args) {
-		System.out.println("hello sagar");
-        System.out.println("hello sravani");
+		System.out.println("Legend blockbuster");
+        System.out.println("Master super hit");
 	}
 
 }
